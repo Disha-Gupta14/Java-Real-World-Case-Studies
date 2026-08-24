e9514c5d4e11761f67e25cdce948fb6ad5fcb278
Disha-Gupta14 <dishagupta37894@iisuniv.ac.in>
2026-08-24 16:49:13 +0530

    Update EmployeeInterfaceMain.java

--- EmployeeInterfaceMain.java
+++ EmployeeInterfaceMain.java
@@ -1,3 +1,6 @@
+//Write a Java program to create an interface Employee with methods for salary and leave calculation, having child classes Teaching, 
+//Non-Teaching and Adobe with their respective salary components. Implement leave deductions based on late marks and annual leaves as
+//given for each employee type.
 import java.util.Scanner;
 public class EmployeeInterfaceMain{
 	public static void main(String[] args){
