4223e94d3ef1f246beeb556e3090b82ba6efe509
Disha-Gupta14 <dishagupta37894@iisuniv.ac.in>
2026-08-24 13:49:26 +0530

    Update PaintingCost.java

--- PaintingCost.java
+++ PaintingCost.java
@@ -1,3 +1,6 @@
+//Write a java program to calculate a room painting cost with material where room size is 10X15ft, two windows- one is 
+//5X5ft and second is 4X4ft, and a door of 6X4ft. Labour cost is Rs.10 and material cost is Rs.30. Calculate painting cost.
+//(Assumed Height of the room as 10)
 public class PaintingCost{
 	public static void main(String[] args){
 		int PaintableArea,TotalCost, AreaOfRoom, AreaOfWindows, AreaOfDoor,LenR=10, BreR=15,HeigR=10, LenD=6, BreD=4;
